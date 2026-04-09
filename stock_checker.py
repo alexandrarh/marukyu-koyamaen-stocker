f76a65830bc40efcad738b65877ea687735a6c53
alexandrarh <alexavndrarh@gmail.com>
2026-04-09 10:24:03 -0700

    Finished base functionality for update_history

--- stock_checker.py
+++ stock_checker.py
@@ -98,16 +98,43 @@ def update_history(information_list):
     '''
     # Opening the history file or creating a new one if it doesn't exist
     matcha_history = pd.read_csv(HISTORY_FILE) if pd.io.common.file_exists(HISTORY_FILE) else pd.DataFrame(columns = ['matcha_product', 'link', 'stock_status', 'last_updated'])
+
+    # List to keep track of products that updated from OOS to IS
+    instock_changed_products = []
     
     for info in information_list:
         # Check if product exists in history
         if info['product_name'] in matcha_history['matcha_product'].values:
             # Check if stock status has changed
             existing_status = matcha_history.loc[matcha_history['matcha_product'] == info['product_name'], 'stock_status'].values[0]
-            if existing_status != info['stock_status']:
-                pass
 
-def notify_user():
+            if existing_status != info['stock_status']:
+                # If product has changed from OOS to IS, add it to the list for notification
+                if (info['stock_status'] == 'In Stock') and (existing_status == 'Out of Stock'):
+                    product_info = {'matcha_product': info['product_name'], 'link': info['link']}
+                    instock_changed_products.append(product_info, ignore_index=True)
+
+                # Changes if stock status has changed (both OOS to IS and IS to OOS)
+                matcha_history.loc[matcha_history['matcha_product'] == info['product_name'], ['stock_status', 'last_updated']] = [info['stock_status'], pd.Timestamp.now()]
+            else:
+                matcha_history.loc[matcha_history['matcha_product'] == info['product_name'], 'last_updated'] = pd.Timestamp.now()
+        else:
+            if info['stock_status'] == 'In Stock':
+                product_info = {'matcha_product': info['product_name'], 'link': info['link']}
+                instock_changed_products.append(product_info, ignore_index=True)
+
+            # If product does not exist in history, add it
+            new_entry = {
+                'matcha_product': info['product_name'],
+                'link': info['link'],
+                'stock_status': info['stock_status'],
+                'last_updated': pd.Timestamp.now()
+            }
+            matcha_history = pd.concat([matcha_history, pd.DataFrame([new_entry])], ignore_index=True)
+
+    return instock_changed_products
+
+def notify_user(instock_changed_products):
     '''
     Notify the user about stock availability.
     '''
@@ -125,6 +152,12 @@ def main():
     information_list = stock_parser(html_source)
     for info in information_list:
         print(f"Item Name: {info['product_name']}, Link: {info['link']}, Stock Status: {info['stock_status']}")
+
+    instock_changed_products = update_history(information_list)
+    if len(instock_changed_products) > 0:
+        notify_user(instock_changed_products)
+    else:
+        logging.info("No products changed from Out of Stock to In Stock.")
 
 if __name__ == "__main__":
     main()
